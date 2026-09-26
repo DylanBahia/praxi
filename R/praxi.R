1b12f0b33d93ec64db261ca2384cc72708af7002
Bahia <bahiad1@vdi-stori-060.lancs.local>
2026-09-26 13:35:02 +0100

    a

--- R/praxi.R
+++ R/praxi.R
@@ -258,7 +258,7 @@ setMethod("plot",signature=list("crops.class"),
               ggplot(.) %>% 
               add(geom_hline(aes(yintercept=dummy))) %>%
               add(geom_segment(aes(x=st,xend=nd,y=dummy,yend=dummy),colour="blue")) %>%
-              add(geom_point(data=subset(.,st==nd),aes(x=st,y=dummy),colour="red")) %>%
+              add(geom_point(data=subset(df,st==nd),aes(x=st,y=dummy),colour="red")) %>%
               add(labs(x="location",y="penalty")) %>%
               add(scale_y_continuous(breaks = seq(1:nrow(df)),labels=signif(df$beta,digits=3),sec.axis = sec_axis( ~.,breaks = seq(1:nrow(df)),labels=signif(df$Qm,digits=4),name="unpenalised cost"))) %>%
               add(theme_bw())
