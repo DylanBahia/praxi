1a880c99b8f2d5efd8a37f6e8eaebadc3e908ddd
Bahia <bahiad1@vdi-stori-060.lancs.local>
2026-09-26 11:45:34 +0100

    a

--- R/praxi.R
+++ R/praxi.R
@@ -1,6 +1,7 @@
 #' @useDynLib praxi, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #' @importFrom magrittr %>%
+#' @importFrom sets set tuple as.tuple set_union set_symdiff set_is_empty as.set
 NULL
 
 .praxi.class <- setClass("praxi.class",representation(y="numeric",p="numeric",b="numeric",cost="numeric",res="matrix"))
